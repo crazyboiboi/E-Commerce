4277721869209865acd95f6f7a5c5aad0e2e2eba
Sean Seow <seow.hongsuen123@gmail.com>
2021-09-20 16:24:39 +0100

    Added and styled Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+// Pages
+import Home from "./pages/Home";
+
+// Components
+import Navbar from "./components/Navbar/Navbar";
+
 const App = () => {
-    return <div>Hello world!</div>;
-  };
-  
-  export default App;
+  return (
+    <>
+    <Router>
+      <Navbar />
+      {/* <Home /> */}
+
+    </Router>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+import { ShoppingCart } from '@material-ui/icons'
+import { Badge } from '@material-ui/core'
+
+import logo from '../../assets/logo.svg'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link to="/">
+                <img src={logo} alt="logo"></img>
+            </Link>
+
+            <ul className="navbar__links">
+                <li>
+                    <Link to="/" className="link">Home</Link>
+                </li>
+                <li>
+                    <Link to="/shop" className="link">Shop</Link>
+                </li>
+                <li>
+                    <Link to="/contact" className="link">Contact</Link>
+                </li>
+                <li>
+                    <Link to="/contact" className="link">
+                        <Badge badgeContent={2} color="primary">
+                            <ShoppingCart />
+                        </Badge>
+                    </Link>
+                </li>
+            </ul>
+
+
+        </nav>
+    )
+}
+
+export default Navbar
+
+
+// To do
+// 1) Add shadow when scrolling
